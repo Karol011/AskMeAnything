731ee270b78b320e4fe951f7756d3153ec17f10d
Karol011 <karollacek1@gmail.com>
2023-09-28 11:46:47 +0200

    initial commit

---
+++ src/main/java/com/example/AskMeAnything/AskMeAnythingApplication.java
@@ -0,0 +1,13 @@
+package com.example.AskMeAnything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskMeAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskMeAnythingApplication.class, args);
+	}
+
+}
