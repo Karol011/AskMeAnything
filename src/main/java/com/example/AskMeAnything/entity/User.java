96168330653578c87ede68c1f884834b7f8e033a
Karol011 <karollacek1@gmail.com>
2023-09-29 14:03:32 +0200

    addedh2database

--- src/main/java/com/example/AskMeAnything/entity/User.java
+++ src/main/java/com/example/AskMeAnything/entity/User.java
@@ -1,15 +1,24 @@
 package com.example.AskMeAnything.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Data
 @AllArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
     private long id;
+    @NonNull
     private String username;
     private String password;
     private String email;
