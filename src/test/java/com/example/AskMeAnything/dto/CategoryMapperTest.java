2e30987954a7f9138cceabf7f59b6894b18f03dd
Karol011 <karollacek1@gmail.com>
2023-12-05 15:52:25 +0100

    created tests for category mapper

---
+++ src/test/java/com/example/AskMeAnything/dto/CategoryMapperTest.java
@@ -0,0 +1,42 @@
+package com.example.AskMeAnything.dto;
+
+import com.example.AskMeAnything.entity.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryMapperTest {
+
+    @Test
+    void shouldReturnEntity() {
+        Long categoryId = 1L;
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setId(categoryId);
+        categoryDto.setName("Test Category");
+        CategoryMapper categoryMapper = new CategoryMapper();
+
+        Category category = categoryMapper.toEntity(categoryDto);
+
+        assertNotNull(category);
+        assertEquals(categoryDto.getId(), category.getId());
+        assertEquals(categoryDto.getName(), category.getName());
+
+    }
+
+    @Test
+    void shouldReturnDTO() {
+
+        Long categoryId = 1L;
+        Category category = new Category();
+        category.setId(categoryId);
+        category.setName("Test Category");
+        CategoryMapper categoryMapper = new CategoryMapper();
+
+        CategoryDto categoryDto = categoryMapper.toDto(category);
+
+        assertNotNull(categoryDto);
+        assertEquals(category.getId(), categoryDto.getId());
+        assertEquals(category.getName(), categoryDto.getName());
+    }
+}
